c9d9456f205c39608a54290ee9a4a433a0df19d7
GURU2003-G <nisapteguruprasad@gmail.com>
2025-11-20 13:48:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.style.display =
+        navLinks.style.display === "flex" ? "none" : "flex";
+});
